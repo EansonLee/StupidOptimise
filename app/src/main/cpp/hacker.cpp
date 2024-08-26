02f8a39c3f638a6780aba1b23db9494da34b0029
liyizhen <liyizhen@xmiles.cn>
2024-08-26 18:04:16 +0800

    修改hook配置

--- app/src/main/cpp/hacker.cpp
+++
@@ -1,3 +0,0 @@
-//
-// Created by xmiles on 2024/8/24.
-//
---
+++ app/src/main/java/com/eason/stupidoptimise/activity/HookActivity.java
@@ -0,0 +1,94 @@
+package com.eason.stupidoptimise.activity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bytedance.android.bytehook.ByteHook;
+import com.eason.stupidoptimise.R;
+import com.eason.stupidoptimise.hook.NativeHacker;
+import com.eason.stupidoptimise.hook.NativeHookee;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class HookActivity extends AppCompatActivity {
+    private String TAG = "bytehook_tag";
+    private int curType = -1; // -1: unhook; 0: hook-single; 1: hook-partial; 2: hook-all
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hook);
+    }
+
+    private void hookOrUnhook(int newType) {
+        if(newType == curType) {
+            return;
+        }
+
+        if(curType != -1) {
+            NativeHacker.unhook();
+            curType = -1;
+        }
+
+        if(newType != -1) {
+            NativeHacker.hook(newType);
+            curType = newType;
+        }
+    }
+
+    public void onRadioButtonClicked(View view) {
+        switch(view.getId()) {
+            case R.id.radio_hook_single:
+                hookOrUnhook(0);
+                break;
+            case R.id.radio_hook_partial:
+                hookOrUnhook(1);
+                break;
+            case R.id.radio_hook_all:
+                hookOrUnhook(2);
+                break;
+            case R.id.radio_unhook:
+                hookOrUnhook(-1);
+                break;
+        }
+    }
+
+    public void onTestClick(View view) {
+        NativeHookee.test();
+    }
+
+    public void onGetRecordsClick(View view) {
+        String records = ByteHook.getRecords();
+        if (records != null) {
+            for (String line : records.split("\n")) {
+                Log.i(TAG, line);
+            }
+        }
+    }
+
+    public void onDumpRecordsClick(View view) {
+        String pathname = getApplicationContext().getFilesDir() + "/bytehook_records.txt";
+        NativeHacker.dumpRecords(pathname);
+
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(pathname));
+            String line;
+            while ((line = br.readLine()) != null) {
+                Log.i(TAG, line);
+            }
+        } catch (Throwable ignored) {
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (Exception ignored) {
+                }
+            }
+        }
+    }
+}
