290a854c5d719a8b6e3abb6e427b89b6efcec68c
liyizhen <liyizhen@xmiles.cn>
2024-09-04 18:41:15 +0800

    新增shadowhook，新增GC抑制

--- hooklib/src/main/cpp/hooklib.cpp
+++ hooklib/src/main/cpp/hooklib.cpp
@@ -1,6 +1,6 @@
 #include <jni.h>
 #include "bytehook.h"
-
+#include "shadowhook.h"
 #include <android/log.h>
 #include <dlfcn.h>
 #include <fcntl.h>
@@ -39,7 +39,6 @@ typedef int (*concurrent_t)(const char *, int, mode_t);
           (uintptr_t)prev_func, (uintptr_t)arg);                                                             \
     }                                                                                                        \
   }
-
 CONCURRENT_DEF(concurrent)
 
 
@@ -55,23 +54,118 @@ static int concurrent_proxy_gc(const char *pathname, int flags, mode_t modes) {
     return fd;
 }
 
+//PLT hook - 这种hook只能hook外部so，没办法hook系统级的so库
+//extern "C"
+//JNIEXPORT jboolean JNICALL
+//Java_com_eason_hooklib_NativeLib_00024Companion_hookConcurrentGCTask(JNIEnv *env, jobject thiz) {
+//
+//    void *concurrent_proxy = (void *) concurrent_proxy_gc;
+//    concurrent_stub = bytehook_hook_single("libart.so", NULL, "Run", concurrent_proxy ,
+//                                           concurrent_hooked_callback, NULL);
+//
+//    concurrent_stub = bytehook_hook_all(NULL, "Run", concurrent_proxy ,
+//                                           concurrent_hooked_callback, NULL);
+//
+//    if (concurrent_stub == nullptr) {
+//        LOG("----------------hook failed------------------ ");
+//        return JNI_FALSE;
+//    } else{
+//        LOG("----------------hook success------------------ ");
+//        return JNI_TRUE;
+//    }
+
+
+// GC
+void *originGC = nullptr;
+void *gcStub = nullptr;
+
+// JIT
+void *originJit = nullptr;
+void *jitStub = nullptr;
+
+typedef void (*TaskRunType)(void *, void *);
+
+void newJit(void *task, void *thread) {
+    if (jitStub != nullptr) {
+        LOG("unhook jit task");
+        shadowhook_unhook(jitStub);
+    }
+    LOG("jit task sleep");
+    sleep(5);
+    LOG("jit task wake up");
+    ((TaskRunType) originJit)(task, thread);
+    LOG("jit task done");
+}
+
+void newGCDelay(void *task, void *thread) {
+    if (gcStub != nullptr) {
+        LOG("unhook gc task");
+        shadowhook_unhook(gcStub);
+    }
+    LOG("gc task sleep");
+    sleep(5);
+    LOG("gc task wake up");
+    ((TaskRunType) originGC)(task, thread);
+    LOG("gc task done");
+}
+
+bool jitDelay() {
+    const char *jit = "_ZN3art3jit14JitCompileTask3RunEPNS_6ThreadE";
+    jitStub = shadowhook_hook_sym_name("libart.so", jit,
+                                       (void *) newJit,
+                                       (void **) &originJit);
+    if (jitStub != nullptr) {
+        LOG("hook JitCompileTask success");
+        return JNI_TRUE;
+    } else {
+        LOG("hook JitCompileTask failed");
+        return JNI_FALSE;
+    }
+}
+
+void gcDelay5X() {
+    // Android 5.X 没有 ConcurrentGCTask, hook ConcurrentGC
+    gcStub = shadowhook_hook_sym_name("libart.so",
+                                      "_ZN3art2gc4Heap12ConcurrentGCEPNS_6ThreadE",
+                                      (void *) newGCDelay,
+                                      (void **) &originGC);
+    if (gcStub != nullptr) {
+        LOG("hook ConcurrentGC success");
+    } else {
+        LOG("hook ConcurrentGC failed");
+    }
+}
+
+void gcDelay() {
+    // Android 5.X 没有 ConcurrentGCTask, hook ConcurrentGC
+    gcStub = shadowhook_hook_sym_name("libart.so",
+                                      "_ZN3art2gc4Heap16ConcurrentGCTask3RunEPNS_6ThreadE",
+                                      (void *) newGCDelay,
+                                      (void **) &originGC);
+    if (gcStub != nullptr) {
+        LOG("hook ConcurrentGCTask success");
+    } else {
+        LOG("hook ConcurrentGCTask failed");
+    }
+}
 
 extern "C"
 JNIEXPORT jboolean JNICALL
 Java_com_eason_hooklib_NativeLib_00024Companion_hookConcurrentGCTask(JNIEnv *env, jobject thiz) {
+    int androidApi = android_get_device_api_level();
+    if (androidApi < __ANDROID_API_M__) { // Android 5.x 版本
+        gcDelay5X();
+    } else if (androidApi < 34) { // Android 6 - 13
+        gcDelay();
+    } else {
+        LOG("android api > 33, return!");
+    }
 
-    void *concurrent_proxy = (void *) concurrent_proxy_gc;
-    concurrent_stub = bytehook_hook_single("libart.so", NULL, "Run", concurrent_proxy ,
-                                           concurrent_hooked_callback, NULL);
 
-//    concurrent_stub = bytehook_hook_all(NULL, "Run", concurrent_proxy ,
-//                                           concurrent_hooked_callback, NULL);
-
-    if (concurrent_stub == nullptr) {
-        LOG("----------------hook failed------------------ ");
+    if (androidApi < __ANDROID_API_N__ || androidApi >= 34) { // Android  < 7  > 13 先不管
+        LOG("android api > 33 || < 24, return!");
         return JNI_FALSE;
-    } else{
-        LOG("----------------hook success------------------ ");
-        return JNI_TRUE;
+    } else { // Android 7 - 13
+        return jitDelay();
     }
 }
--- hooklib/src/main/java/com/eason/hooklib/manager/GcManager.kt
+++ hooklib/src/main/java/com/eason/hooklib/manager/GcManager.kt
@@ -1,18 +1,27 @@
 package com.eason.hooklib.manager
 
 import android.util.Log
-import com.bytedance.android.bytehook.ByteHook
+import com.bytedance.shadowhook.ShadowHook
+
 
 object GcManager {
 
     @JvmStatic
     @Synchronized
     fun init() {
-        val res = ByteHook.init(
-            ByteHook.ConfigBuilder()
-                .setMode(ByteHook.Mode.AUTOMATIC)
-                .setDebug(true)
-                .setRecordable(true)
+        // plthook
+//        val res = ByteHook.init(
+//            ByteHook.ConfigBuilder()
+//                .setMode(ByteHook.Mode.AUTOMATIC)
+//                .setDebug(true)
+//                .setRecordable(true)
+//                .build()
+//        )
+
+    // shadowhook
+       val res =  ShadowHook.init(
+            ShadowHook.ConfigBuilder()
+                .setMode(ShadowHook.Mode.UNIQUE)
                 .build()
         )
         Log.e("hook", "init $res")
